951aa393e85133d7fa792f47c138981f85f1523f
Aleksei-Uzin <aleksei.uzin@gmail.com>
2023-05-18 12:24:51 +0400

    solurions

---
+++ 8-kuy/Chuck Norris VII - True or False.js
@@ -0,0 +1,9 @@
+/**
+ * Chuck Norris VII - True or False? (Beginner)
+ * https://www.codewars.com/kata/570669d8cb7293a2d1001473/train/javascript
+ *
+ */
+
+function ifChuckSaysSo(){
+  return !true
+}
