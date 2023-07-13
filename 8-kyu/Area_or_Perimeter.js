0561e168da32a5c943ac9ca515c42a3e18602e27
Aleksei-Uzin <aleksei.uzin@gmail.com>
2023-07-14 01:24:09 +0400

    solutions

---
+++ 8-kyu/Area_or_Perimeter.js
@@ -0,0 +1,7 @@
+/**
+ * Area or Perimeter
+ * https://www.codewars.com/kata/5ab6538b379d20ad880000ab/train/javascript
+ *
+ */
+
+const areaOrPerimeter = (l , w) => l === w ? (l * w) : ((l + w) * 2)
