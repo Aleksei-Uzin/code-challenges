528826df949432ac36832b36510c4544283260b2
Aleksei-Uzin <aleksei.uzin@gmail.com>
2023-07-22 22:06:50 +0400

    solutions

---
+++ 8-kyu/Capitalization_and_Mutability.js
@@ -0,0 +1,7 @@
+/**
+ * Capitalization and Mutability
+ * https://www.codewars.com/kata/595970246c9b8fa0a8000086/train/javascript
+ *
+ */
+
+const capitalizeWord = word => word.replace(/^[a-z]/, word[0].toUpperCase())
