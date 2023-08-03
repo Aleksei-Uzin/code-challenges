b5cf059ff40f530faa7817ce04f83194683d9612
Aleksei-Uzin <aleksei.uzin@gmail.com>
2023-08-03 21:17:33 +0400

    solutions

---
+++ 8-kyu/Are_arrow_functions_odd.js
@@ -0,0 +1,8 @@
+/**
+ * Are arrow functions odd?
+ * https://www.codewars.com/kata/559f80b87fa8512e3e0000f5/train/javascript
+ *
+ */
+
+const compare = n => n % 2
+const odds = values => values.filter(compare)
---
+++ 8-kyu/Do_you_speak_English.js
@@ -0,0 +1,7 @@
+/**
+ * Do you speak "English"?
+ * https://www.codewars.com/kata/58dbdccee5ee8fa2f9000058/train/javascript
+ *
+ */
+
+const spEng = sentence => sentence.toLowerCase().includes('english')
