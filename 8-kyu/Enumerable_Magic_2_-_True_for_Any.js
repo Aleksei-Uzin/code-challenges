487e19a0b704e634c2b72667e71693f15b6f5356
Aleksei-Uzin <aleksei.uzin@gmail.com>
2023-08-05 00:05:13 +0400

    solutions

---
+++ 8-kyu/Enumerable_Magic_2_-_True_for_Any.js
@@ -0,0 +1,7 @@
+/**
+ * Enumerable Magic #2 - True for Any?
+ * https://www.codewars.com/kata/54598e89cbae2ac001001135/train/javascript
+ *
+ */
+
+const any = (arr, fun) => arr.some(fun)
---
+++ 8-kyu/Exclamation_marks_series_11_Replace_all_vowel_to_exclamation_mark_in_the_sentence.js
@@ -0,0 +1,7 @@
+/**
+ * Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence
+ * https://www.codewars.com/kata/57fb09ef2b5314a8a90001ed/train/javascript
+ *
+ */
+
+const replace = s => s.replace(/[aeoiu]/ig, '!')
