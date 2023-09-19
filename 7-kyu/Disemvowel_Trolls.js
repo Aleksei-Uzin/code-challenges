53668ece18c992a1baddac51551332ec8c037281
Aleksei-Uzin <aleksei.uzin@gmail.com>
2023-09-19 23:28:43 +0400

    solutions

---
+++ 7-kyu/Disemvowel_Trolls.js
@@ -0,0 +1,7 @@
+/**
+ * Disemvowel Trolls
+ * https://www.codewars.com/kata/52fba66badcd10859f00097e/train/javascript
+ *
+ */
+
+const disemvowel = str => str.replace(/a|e|i|o|u/gi, '')
--- 7-kyu/Fizz_Buzz.js
+++ 7-kyu/Fizz_Buzz.js
@@ -4,7 +4,24 @@
  *
  */
 
+// A
 const fizzbuzz = n => {
+  const res = []
+  
+  for(let i = 1; i <= n; i++) {
+    let str = ''
+
+    if(i % 3 === 0) str += 'Fizz'
+    if(i % 5 === 0) str += 'Buzz'
+
+    res.push(str || i)
+  }
+  
+  return res
+}
+
+// B
+const _fizzbuzz = n => {
   const res = []
   
   for(let i = 1; i <= n; i++) {
