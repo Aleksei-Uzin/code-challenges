73f15e46bed1bfa4c726ecbb6a58fcfe6a119b49
Aleksei-Uzin <aleksei.uzin@gmail.com>
2024-03-07 23:19:13 +0400

    solution

--- 01-JavaScript/2-kyu/Evaluate_mathematical_expression.js
+++ 01-JavaScript/2-kyu/Evaluate_mathematical_expression.js
@@ -8,32 +8,24 @@ const calc = exp => {
   const parensSumOrSub = /\((-?\s?\d+\.?\d*)\s*(\+|\-)\s*(-?\s?\d+\.?\d*)\)/g
   const multOrDiv = /(-?\s?\d+\.?\d*)\s*(\*|\/)\s*(-?\s?\d+\.?\d*)/g
   const sumOrSub = /(-?\s?\d+\.?\d*)\s*(\+|\-)\s*(-?\s?\d+\.?\d*)/g
-  const parens = /\((-?\s?\d+\.?\d*)\)/g
-  const invertSign = /\-\-(\d+\.?\d*)/g
 
-  exp = exp.replace(parens, '$1')
-  exp = exp.replace(invertSign, '$1')
+  exp = exp.replace(/\((-?\s?\d+\.?\d*)\)/g, '$1')
+  exp = exp.replace(/\-\-(\d+\.?\d*)/g, '$1')
 
   while (parensSumOrSub.test(exp)) {
     exp = exp.replace(parensSumOrSub, (_, n1, sign, n2) =>
       makeCalc(+n1, +n2, sign)
     )
-    exp = exp.replace(parens, '$1')
-    exp = exp.replace(invertSign, '$1')
   }
 
   while (multOrDiv.test(exp)) {
     exp = exp.replace(multOrDiv, (_, n1, sign, n2) => makeCalc(+n1, +n2, sign))
-    exp = exp.replace(parens, '$1')
-    exp = exp.replace(invertSign, '$1')
   }
 
   if (parensSumOrSub.test(exp)) return calc(exp)
 
   while (sumOrSub.test(exp)) {
     exp = exp.replace(sumOrSub, (_, n1, sign, n2) => makeCalc(+n1, +n2, sign))
-    exp = exp.replace(parens, '$1')
-    exp = exp.replace(invertSign, '$1')
   }
 
   return isNaN(exp) ? calc(exp) : +exp
